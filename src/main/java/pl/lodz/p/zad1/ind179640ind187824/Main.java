da81c222df886b282032cf9b27c3414a5f49f324
lukasz switon <lukas06.04.93@gmail.com>
2016-10-11 12:08:56 +0200

    Added string requesting

--- src/main/java/pl/lodz/p/zad1/ind179640ind187824/Main.java
+++ src/main/java/pl/lodz/p/zad1/ind179640ind187824/Main.java
@@ -1,10 +1,17 @@
 package pl.lodz.p.zad1.ind179640ind187824;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-
-
+		String[] data = new String[3];
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Podaj nazwe");
+		data[0] = scanner.nextLine();
+		System.out.println("Podaj poprawny string:");
+		data[1] = scanner.nextLine();
+		System.out.println("Podaj niepoprawny string:");
+		data[2] = scanner.nextLine();
 	}
-
 }
